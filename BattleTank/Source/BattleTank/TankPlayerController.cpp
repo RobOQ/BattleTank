05328744176bad7fa70c05a19576a1b68c995f42
Rob O'Quinn <rob.oquinn@gmail.com>
2017-10-11 21:19:00 -0300

    Testing the ability to get our controlled tank from the TankPlayerController

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,9 +2,22 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	ATank* controlledTank = GetControlledTank();
+
+	if (controlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play possessing %s"), *(controlledTank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play not possessing a tank"));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
-
-
