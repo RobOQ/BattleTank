4fc1e1021ea6116d98bf0edfdc795c427c273f7e
Rob O'Quinn <rob.oquinn@gmail.com>
2017-10-16 20:43:43 -0300

    Calculating the projectile aim direction

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -1,6 +1,8 @@
 // Copyright 2017 Robert O'Quinn
 
 #include "TankAimingComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/StaticMeshComponent.h"
 
 
 // Sets default values for this component's properties
@@ -38,8 +40,20 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 
 void UTankAimingComponent::AimAt(FVector hitLocation, float launchSpeed)
 {
-	auto ourTankName = GetOwner()->GetName();
-	auto barrelLocation = barrel->GetComponentLocation();
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at: %s from %s"), *ourTankName, *hitLocation.ToString(), *barrelLocation.ToString());
+	if (!barrel)
+	{
+		return;
+	}
+
+	FVector outLaunchVelocity = FVector(0.0f);
+	FVector startLocation = barrel->GetSocketLocation(FName("Projectile"));
+	
+	// Calculate the outLaunchVelocity
+	if (UGameplayStatics::SuggestProjectileVelocity(this, outLaunchVelocity, startLocation, hitLocation, launchSpeed, false, 0.f, 0, ESuggestProjVelocityTraceOption::DoNotTrace))
+	{
+		auto aimDirection = outLaunchVelocity.GetSafeNormal();
+		auto tankName = GetOwner()->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *tankName, *aimDirection.ToString())
+	}
 }
 
