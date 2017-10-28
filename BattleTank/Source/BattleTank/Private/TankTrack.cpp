9ecbe0bacfa7e634c673adafb2a7a5069df3c8c1
Rob O'Quinn <rob.oquinn@gmail.com>
2017-10-28 16:20:38 -0300

    Track applies a force to the tank body

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -4,8 +4,12 @@
 
 void UTankTrack::SetThrottle(float throttle)
 {
-	auto name = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *name, throttle);
+	throttle = FMath::Clamp(throttle, -1.0f, 1.0f);
+	auto forceApplied = GetForwardVector() * throttle * trackMaxDrivingForce;
+	auto forceLocation = GetComponentLocation();
+	auto tankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	tankRoot->AddForceAtLocation(forceApplied, forceLocation);
 
-	// TODO clamp actual throttle value so player can't drive faster than allowed
+	auto name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s forceApplied: %s"), *name, *forceApplied.ToString());
 }
